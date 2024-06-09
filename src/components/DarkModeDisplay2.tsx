a3663f6b25c508d809f8beefb129090443908d2f
Zach Lankton <zach.lankton@freightverify.com>
2024-06-09 12:35:24 -0400

    default value not required

--- src/components/DarkModeDisplay2.tsx
+++ src/components/DarkModeDisplay2.tsx
@@ -1,6 +1,6 @@
 import { usePersistentKvState } from "../hooks/useKvState";
 
 export function DarkModeDisplay2() {
-  const [darkMode] = usePersistentKvState("darkMode", true);
+  const [darkMode] = usePersistentKvState("darkMode");
   return <p>{darkMode ? "Dark Mode" : "Light Mode"}</p>;
 }
