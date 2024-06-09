98bebaf4403f3e571505efde8f4baf8170ddb21a
Zach Lankton <zach.lankton@freightverify.com>
2024-06-09 13:33:18 -0400

    sort of working ... initial value for first input is wrong and memory leak

--- src/hooks/useKvState.ts
+++ src/hooks/useKvState.ts
@@ -1,34 +1,41 @@
 import { useState, useEffect } from "react";
 
-interface KVStore {
-  [key: PropertyKey]: any;
-}
-
-interface Listeners {
-  [key: PropertyKey]: ((value: any) => void)[];
-}
-
 interface KvStoreOptions {
   persistent?: boolean;
   localStorageKey?: string;
+  trackAndIsolate?: boolean;
 }
 
 export const createKvStore = (
-  { persistent, localStorageKey }: KvStoreOptions = {
+  { persistent, localStorageKey, trackAndIsolate }: KvStoreOptions = {
     persistent: false,
     localStorageKey: "usePersistentKvStateDefaultLocalStorageKey",
+    trackAndIsolate: false,
   }
 ) => {
   const kvStore = new Map();
   const listeners = new Map();
+  const scopeTracking = new Map();
+  let count = 0;
 
   const useKVStore = <K extends PropertyKey, T>(
-    key: K,
-    initialValue: T
+    propKey: K,
+    initialValue?: T
   ): [T, (newValue: T) => void] => {
+    count++;
+    let key = propKey;
+    const newScope = {
+      count,
+      propKey,
+      initialValue,
+    };
+    const [thisScope, setScope] = useState<any>(null);
     const lsKey = `${localStorageKey}.${key.toString()}`;
 
     const [state, setState] = useState<T>(() => {
+      if (trackAndIsolate) {
+        console.log("setting state", key, initialValue, kvStore.has(key));
+      }
       if (kvStore.has(key)) {
         return kvStore.get(key);
       }
@@ -38,27 +45,106 @@ export const createKvStore = (
         return kvStore.get(key);
       }
 
+      if (trackAndIsolate) {
+        return "something";
+      }
+
       kvStore.set(key, initialValue);
       persistent && localStorage.setItem(lsKey, JSON.stringify(initialValue));
-
       return kvStore.get(key);
     });
 
-    useEffect(() => {
+    const setupScope = () => {
+      if (trackAndIsolate && initialValue !== undefined) {
+        key = scopeTracking.get(propKey);
+        scopeTracking.delete(propKey);
+        console.log("setting scope (top level)", key);
+        if (key === undefined) debugger;
+        setScope(key);
+        setKVStore(initialValue);
+      }
+
+      if (trackAndIsolate && initialValue === undefined) {
+        if (scopeTracking.has(key)) {
+          console.log("setting scope", newScope);
+          key = scopeTracking.get(key);
+          setScope(key);
+        } else {
+          console.log("setupScope", newScope);
+          scopeTracking.set(propKey, newScope);
+          kvStore.set(newScope, initialValue);
+          //@ts-ignore
+          key = newScope;
+          setScope(newScope);
+        }
+      }
+    };
+
+    const cleanupScope = () => {
+      if (trackAndIsolate && initialValue !== undefined) {
+        console.log(
+          "cleanupScope",
+          kvStore.size,
+          scopeTracking.size,
+          listeners.size,
+          thisScope
+        );
+        scopeTracking.delete(key);
+        kvStore.delete(thisScope);
+        // listeners.set(thisScope, []);
+      }
+    };
+
+    const setupListeners = () => {
+      if (trackAndIsolate && initialValue === undefined) {
+        console.log("setupListeners", key);
+      }
       if (!listeners.has(key)) {
         listeners.set(key, []);
       }
       listeners.get(key).push(setState);
+    };
 
-      return () => {
+    const cleanupListeners = () => {
+      if (trackAndIsolate && initialValue !== undefined) {
+        console.log("cleanupListeners", key);
+      }
+
+      if (listeners.has(key)) {
         listeners.set(
           key,
           listeners.get(key).filter((listener: any) => listener !== setState)
         );
+
+        if (listeners.get(key).length === 0) {
+          listeners.delete(key);
+        }
+      }
+    };
+
+    useEffect(() => {
+      setupScope();
+      setupListeners();
+
+      return () => {
+        cleanupScope();
+        cleanupListeners();
       };
     }, [key]);
 
     const setKVStore = (newValue: T) => {
+      if (trackAndIsolate) {
+        if (thisScope) {
+          key = thisScope;
+        }
+        console.log("setting scope and value", key);
+        if (key === null) debugger;
+        if (!key) return;
+      }
+      if (key === "inputValue") {
+        console.log("FUCK");
+        debugger;
+      }
       kvStore.set(key, newValue);
       persistent && localStorage.setItem(lsKey, JSON.stringify(newValue));
       listeners.get(key).forEach((listener: any) => listener(newValue));
@@ -72,3 +158,6 @@ export const createKvStore = (
 
 export const useKvState = createKvStore();
 export const usePersistentKvState = createKvStore({ persistent: true });
+export const useScopedKvState = createKvStore({
+  trackAndIsolate: true,
+});
